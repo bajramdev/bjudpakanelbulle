551d1749e248e24bf5b0cc1d7aa232749d59f30f
bajramdev <arfiesty0@gmail.com>
2020-12-03 22:59:32 +0100

    Fixed query from nodejs  to db

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,44 @@
 const express = require('express');
 const mysql = require('mysql');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+
+
+dotenv.config({ path: './.env' })
 
 const app = express();
 
+app.use(cors());
+app.use(express.json())
+
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'nodejs-login'
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABAS_PASSWORD,
+    database: process.env.DATABASE
 });
+
+
+app.post('/create', (req,res) => {
+    console.log(res.body)
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    db.query('INSERT INTO users (name, email, password) VALUES (?,?,?),' [name, email, password],
+        (err, result) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            res.send("Values Inserted" + result)
+          }
+        }
+    );
+})
+
+
 
 db.connect( (error) => {
     if (error){
@@ -22,6 +52,6 @@ app.use('/' , (req,res) => {
     res.send("<h1> Home Page</h1>");
 })
 
-app.listen(8112, () => {
-    console.log("Server started on Port 8000")
+app.listen(3001, () => {
+    console.log("Server started on Port 8112")
 })
